8000eef9d18a4f44e4090ef540a70f155182eb33
Nelson Faria <49026704+nelson31@users.noreply.github.com>
2021-05-29 11:16:59 +0100

    add admin

--- http-server/apphttp.py
+++ http-server/apphttp.py
@@ -11,7 +11,7 @@ Desenvolvido por: Nelson Faria (a84727@alunos.uminho.pt)
 
 import os, sys, datetime
 import requests
-import jwt, json
+import jwt, json, socket
 from werkzeug.utils import secure_filename
 from flask import Flask, request, url_for, redirect, render_template, make_response, send_from_directory, current_app
 from dotenv import load_dotenv
@@ -28,9 +28,9 @@ EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg'])
 app.config['UPDIRECTORY'] = UPDIRECTORY
 
 # IP do servidor de autenticacao
-auth_ip = "172.20.0.3"
+auth_ip = socket.gethostbyname("auth_container")
 # IP do servidor de HTTP
-http_ip = "172.20.0.2"
+http_ip = socket.gethostbyname("http_container")
 # Porta do servidor autenticacao
 auth_port = 5000
 # Porta do servidor HTTP
